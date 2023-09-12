b09a8eb907dc6430335d798f97c555c4916bc8e6
baberparweez <baberparweez@gmail.com>
2023-09-12 10:40:13 -0300

    change setup to use bun 1.1

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,16 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "/",
-    build: {
-        assetsInlineLimit: 0,
-        rollupOptions: {
-            output: {
-                entryFileNames: `[name].js`,
-                chunkFileNames: `[name].js`,
-                assetFileNames: `[name].[ext]`,
-            },
-        },
-    },
+    // build: {
+    //     assetsInlineLimit: 0,
+    //     rollupOptions: {
+    //         output: {
+    //             entryFileNames: `[name].js`,
+    //             chunkFileNames: `[name].js`,
+    //             assetFileNames: `[name].[ext]`,
+    //         },
+    //     },
+    // },
     plugins: [
         react(),
         // {
